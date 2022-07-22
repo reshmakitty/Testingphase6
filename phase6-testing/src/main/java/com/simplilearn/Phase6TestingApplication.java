f2cd062a7de46bdb88b5444ea1ef06d46f6a63f4
reshmakitty <98320714+reshmakitty@users.noreply.github.com>
2022-07-21 22:46:46 -1200

    Testing

---
+++ phase6-testing/src/main/java/com/simplilearn/Phase6TestingApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase6TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase6TestingApplication.class, args);
+	}
+
+}
---
+++ phase6-testing/src/test/java/com/simplilearn/phase6/testing/Phase6TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.phase6.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase6TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
